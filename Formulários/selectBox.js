79ac27bbe42d6f36997a849da431e031c87165d1
Gabriel <email@gabrielfelipe.com>
2013-08-22 08:28:42 -0300

    Update License, author was wrong.

--- Formulários/selectBox.js
+++ Formulários/selectBox.js
@@ -1,7 +1,7 @@
 /*
 * ----------------------------------------------------------------------------
 * "THE BEER-WARE LICENSE" (Revision 42):
-* <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+* <gabriel@webingpro.com.br> wrote this file. As long as you retain this notice you
 * can do whatever you want with this stuff. If we meet some day, and you think
 * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
 * ----------------------------------------------------------------------------
@@ -24,7 +24,17 @@
 select = new selectBox('.pergunta.select');
 
 Suporte a navegação por teclado.
+
+Exemplo de CSS:
+form .select {cursor: pointer; position: relative; width:300px;}
+form .select .seta {position: absolute; top:4px; right: 4px; width:30px; height: 30px; .border-radius(3px); background: #820; color:white; line-height: 30px; text-align: center;font-size:17px;}
+form .select .opcoes {position: absolute; top:100%; width:100%; background: #fff; left:0;border: 1px solid #e3e3e3; border-top:none; .box-shadow(0px 1px 6px rgba(0,0,0,0.11) inset); .border-radius(0 0 3px 3px)}
+form .select .opcoes button {width:100%; display: block; border:none; background: #fff; box-shadow: none; text-align: left;cursor:pointer; .border-radius(0); margin:0;}
+form .select .opcoes button:last-child {.border-radius(0 0 3px 3px); }
+form .select .opcoes button.on {color:#820;}
+form .select label {margin: 0; width:100%; cursor: pointer;}
 */
+
 //--=-=-=-=-SELECT SINGLE //
 function selectBox(obj){
 //Construindo.
